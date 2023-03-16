b71e3ef8a7afdbbb8460350482906aeee97f150e
Coudaa <alfonsusjericho31@gmail.com>
2023-03-16 10:52:05 +0700

    binding

--- app/src/main/java/com/alfonsusjericho202102286/edittextbutton/MainActivity.java
+++ app/src/main/java/com/alfonsusjericho202102286/edittextbutton/MainActivity.java
@@ -8,23 +8,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.alfonsusjericho202102286.edittextbutton.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        Button okButton = (Button) findViewById(R.id.okButton);
-
-        okButton.setOnClickListener(new View.OnClickListener() {
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.okButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText namaEditText = (EditText) findViewById(R.id.namaEditText);
-                String nama = namaEditText.getText().toString();
+                String nama = binding.namaEditText.getText().toString();
                 String pesan = "Woi, " + nama + "!";
-                TextView infoTextView = (TextView) findViewById(R.id.infoTextView);
-                infoTextView.setText(pesan);
+                binding.infoTextView.setText(pesan);
             }
         });
     }
